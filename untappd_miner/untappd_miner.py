573941bbfffb012ea44e87f779b923f2d0b5b301
simlal <simlalonde@hotmail.com>
2023-11-30 14:15:24 -0500

    Fix ua on init and implement get countries

--- untappd_miner/untappd_miner.py
+++ untappd_miner/untappd_miner.py
@@ -1,7 +1,10 @@
 from dotenv import dotenv_values
 from pathlib import Path
-import httpx
 from typing import Optional, Union
+
+import httpx
+from bs4 import BeautifulSoup
+
 class UntappdMiner:    
     def __init__(self, dotenv_file: str | None = None) -> None:
         self.dotenv_file = dotenv_file
@@ -31,7 +34,14 @@ class UntappdMiner:
         try:
             res = httpx.get(url=url, headers=headers, params=params)
             res.raise_for_status()
-            return res
+            
+            content_type = res.headers["content-type"]
+            if "text/html" in content_type:
+                return res.text
+            elif "application/json":
+                return res.json()
+            else:
+                return res
         except httpx.RequestError as e:
             print(f"An error occured for request: {e}")
             raise e
@@ -48,7 +58,7 @@ class UntappdMiner:
         
 
 class UntappdApiMiner(UntappdMiner):
-    BASE_API_URL = "https://api.untappd.com"
+    BASE_API = "https://api.untappd.com"
     
     def __init__(self, dotenv_file: str | None = None) -> None:
         super().__init__(dotenv_file)
@@ -71,14 +81,16 @@ class UntappdApiMiner(UntappdMiner):
         }        
         
         res = self._fetch_url(url, params=params)
-        return res.json()
+        return res
     
 class UntappdWebMiner(UntappdMiner):
-    BASE_URL = "https://untappd.com/"
+    BASE_URL = "https://untappd.com"
+    BEER_TR_ENDPOINT = "/beer/top_rated"
+    BREWERY_TR_ENDPOINT = "/brewery/top_rated"
     
-    def __init__(self, dotenv_file: str | None = None, custom_ua: str | None = None) -> None:
+    def __init__(self, dotenv_file: str | None = None, user_agent: str | None = None) -> None:
         super().__init__(dotenv_file)
-        self.user_agent = custom_ua    # Set a default UA if none provided
+        self._user_agent = self.__ua_setter_on_init(user_agent)    # Set a default UA if none provided
         
     @property
     def user_agent(self) -> str:
@@ -86,17 +98,46 @@ class UntappdWebMiner(UntappdMiner):
     
     @user_agent.setter
     def user_agent(self, custom_ua: str | None):
-        # Required for proper parsing
-        print(self.parse_dotenv(self._dotenv_file, "USER_AGENT"))
-        if custom_ua is None and self.parse_dotenv(self._dotenv_file, "USER_AGENT") is None:
-            custom_ua = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0"
-        else:
-            custom_ua = self.parse_dotenv(self._dotenv_file, "USER_AGENT")
-        
         self._user_agent = custom_ua
         
-    # Get beer-related data for a single brewery
-    def get_beers_per_brewery(self, brewery_name: str) -> dict[str, str]:
-        beer_endpoint=f"{self.BASE_URL}{brewery_name}/beer"
-        res = self._fetch_url(beer_endpoint)
-        print(res.status_code)
+    def get_top_rated_breweries(self, ) -> dict[str, str]:
+        pass
+        
+    def _get_countries_slug(self, endpoint: str) -> list[str]:
+        # Validate and construct endpoint
+        endpoints = ["beer", "brewery"]
+        if endpoint not in endpoints:
+            raise ValueError(f"'endpoint' must be one of {endpoints}")
+        
+        if endpoint == endpoint[0]:
+            url = self.BASE_URL + self.BEER_TR_ENDPOINT
+        
+        if endpoint == endpoints[1]:
+            url = self.BASE_URL + self.BREWERY_TR_ENDPOINT
+        
+        # Get source from given endpoint
+        headers = {"User-Agent": self._user_agent}
+        html = self._fetch_url(url=url, headers=headers)
+        
+        # Fetch countries from endpoint
+        soup = BeautifulSoup(html, "html.parser")
+        selecter_country_id = soup.find(id="sort_picker")
+
+        countries = []
+        for option in selecter_country_id.find_all("option"):
+            if "data-value-slug" in option.attrs:
+                countries.append(option["data-value-slug"])
+        return countries
+        
+    def __ua_setter_on_init(self, ua_init: str | None) -> str:
+        # Set default otherwise pick from .env
+        if ua_init is None and self.parse_dotenv(self._dotenv_file, "USER_AGENT") is None:
+            ua = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0"
+        elif ua_init is None:
+            ua = self.parse_dotenv(self._dotenv_file, "USER_AGENT")
+        else:    # Hardcoded ua
+            ua = ua_init
+        return ua
+        
+        
+        
